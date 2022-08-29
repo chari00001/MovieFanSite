36acf406b651db0b9bc441912aad9b48f5248ae3
chari00001 <90192334+chari00001@users.noreply.github.com>
2022-08-29 15:27:03 +0300

    v1.1

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var helmet = require('helmet')
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.use(helmet())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
