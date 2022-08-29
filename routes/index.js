3bbfa29679cdcc9e35643069f1bbe37aecea261a
chari00001 <90192334+chari00001@users.noreply.github.com>
2022-08-29 15:41:40 +0300

    v1.1.1

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var request = require('postman-request')
+
+const apiKey = '1fb720b97cc13e580c2c35e1138f90f8'
+const apiBaseUrl = 'http://api.themoviedb.org/3';
+const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
+const imageBaseUrl = 'http://image.tmdb.org/t/p/w300';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  /**
+   * request.get takes 2 args:
+   * 1. it takes the url to http "get"
+   * 2. the callback to run when done: takes 3 args:
+   *  a. error (if any)
+   *  b. http response
+   *  c. json/data the server sent back
+   */
+  request.get(nowPlayingUrl, (error, response, movieData) => {
+    console.log("====== ERROR ====== ");
+    console.log(error);
+    console.log("====== RESPONSE ======");
+    console.log(response);
+  })
   res.render('index', {  });
 });
 
